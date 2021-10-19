78fd1ae93edc4a25d8e4f9052cc6261049980b93
softsamiul <softsamiul@gmail.com>
2021-10-19 23:06:02 +0530

    404 page modified

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404!</h2>
-            <p>Opps! Page Not Found</p>
+        <div className="text-center w-10/12 mx-auto my-36 ">
+            <h2 className="text-3xl text-red-500">404!</h2>
+            <p className="text-5xl text-red-500 font-extrabold">Opps! Page Not Found</p>
+            <NavLink to="/"> <button className="py-1 px-6 bg-blue-900 rounded mt-6 text-white">Back to home</button> </NavLink>
         </div>
     );
 };
