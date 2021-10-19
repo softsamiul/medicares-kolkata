8f7050faa2bd44e8a5a8249bd178a0ec4e582965
softsamiul <softsamiul@gmail.com>
2021-10-19 21:20:32 +0530

    single service modified

--- src/shared/SharedServices/SingleService/ServiceDetails/ServiceDetails.js
+++ src/shared/SharedServices/SingleService/ServiceDetails/ServiceDetails.js
@@ -14,11 +14,11 @@ const ServiceDetails = () => {
     
     
     return (
-        <div className="flex w-11/12 mx-auto items-center my-12">
-            <div className="w-2/5">
+        <div className="flex flex-col md:flex-row w-8/12 mx-auto items-center my-12">
+            <div className="w-full md:w-2/5">
                 <img src={serviceImg} alt="" />
             </div>
-            <div className="w-3/5">
+            <div className="w-full md:w-3/5 text-left pl-4">
                 <h3 className="text-3xl font-bold mb-2">{serviceName}</h3>
                 <p className="text-left">{serviceDesc}</p>
             </div>
